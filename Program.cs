fd2eed5cb74efd767abb7c9f6ef8f31729e40b19
Philippe Ryf <philippe.ryf@face-it.solutions>
2024-11-07 17:25:53 +0100

    POC for sending the listened music

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
+using CommonConnector.Contracts;
+using DiscordConnector;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
 
 // Add services to the container.
+builder.Services.RegisterDiscordConnector();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +25,38 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
+app.MapGet("/listening", async (IPublicationConnector connector, IConfiguration configuration, [FromQuery(Name = "m")] string? message) => {
+    var token = configuration["Discord:Token"];
+
+    _ = await connector.Connect(token!);
+    _ = await connector.UpdateStatus(message ?? "Ma musique", PublicationTargets.All);
+
+}).WithName("SetListening")
 .WithOpenApi();
+
+app.MapGet("/discord/bot/status", async (IPublicationConnector connector, IConfiguration configuration, [FromQuery(Name = "m")] string? message) => {
+    var token = configuration["Discord:Token"];
+
+    _ = await connector.Connect(token!);
+    _ = await connector.UpdateStatus(message ?? "Ma musique", PublicationTargets.All);
+
+}).WithName("SetDiscordBotStatus")
+.WithOpenApi();
+
+
+
+//app.MapGet("/discord/status", async (IPublicationConnector connector, IConfiguration configuration, [FromQuery(Name ="m")]string? message) => {
+//    var token = configuration["Discord:Token"];
+
+//    _ = await connector.Connect(token!);
+//    _ = await connector.UpdateStatus(message ?? "Hello, World!");
+//    //await Task.Delay(500);
+//    //await connector.Disconnect();
+
+//}).WithName("SetStatusMessage")
+//.WithOpenApi();
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
